a16cf367bec086ec3e407f15fcaf4e940e7eca72
Garrett Steed <steed.garrett@gmail.com>
2021-03-04 18:30:59 -0700

    api call working

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar"
 import Search from "./components/Search"
-import SearchResult from "./components/SearchResult"
+// import SearchResult from "./components/SearchResult"
 import AboutUs from "./components/AboutUs"
 import Trending from "./components/Trending"
 import DumpStuff from "./components/DumpStuff"
+// import unsplash from "./Api";
+// import { toJson } from "unsplash-js";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-
+      {/* <Route exact path="/" component={Home} />
+      <Route exact path="/Login" component={""} />
+      <Route exact path="/Signup" component={""} />
+      <Route exact path="/User" component={""} />
+      <Route exact path="/DumpStuff" component={""} /> */}
       <Search />
 
-      <SearchResult />
+      {/* <SearchResult /> */}
 
       <Trending />
 
@@ -24,6 +32,7 @@ function App() {
       <AboutUs />
     
     </div>
+    </Router>
   );
 }
 
