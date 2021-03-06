448d00016a697a0f461b68a75577fe6e77bb56a7
Garrett Steed <steed.garrett@gmail.com>
2021-03-06 13:41:26 -0700

    photo search working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar"
 import Search from "./components/Search"
-// import SearchResult from "./components/SearchResult"
+import SearchInput from "./components/SearchResult"
 import AboutUs from "./components/AboutUs"
 import Trending from "./components/Trending"
 import DumpStuff from "./components/DumpStuff"
@@ -12,30 +12,27 @@ import LoginForm from "./components/Forms/LoginForm"
 
 
 function App() {
+  const [data, setData] = useState([])
   return (
     <Router>
       <div className="App">
         <Navbar />
-        {/* <Route exact path="/" component={Home} /> */}
-        {/* <Route exact path="/Login" component={LoginForm} /> */}
-        {/* <Route exact path="/Signup" component={SignUp} /> */}
-        {/* <Route exact path="/User" component={""} />
-        <Route exact path="/DumpStuff" component={""} /> */}
-        <Search />
+        <Search setData={setData}/>
 
-        {/* <SearchResult /> */}
-
-      {/* <SearchResult /> */}
+        <SearchInput setData = {setData} data={data}/>
 
         <Trending />
        
         <DumpStuff />
 
-
         <AboutUs />
+        <Switch>
         <Route exact path="/Login" component={LoginForm} />
-
-         
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route exact path="/Signup" component={SignUp} /> */}
+        {/* <Route exact path="/User" component={""} />
+        <Route exact path="/DumpStuff" component={""} /> */}
+        </Switch>  
     
     </div>
     </Router>
