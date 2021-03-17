b56b268884ba2547cccabb73bbef1b5f1791d1d8
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-03-16 22:32:18 -0500

    merging

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -34,18 +34,6 @@ export default function ButtonAppBar() {
 
   return (
     <Grid className={classes.style}>
-<<<<<<< HEAD
-    <div className={classes.root}>
-      <AppBar position="static" className={classes.style}>
-        <Toolbar>
-          <Link to="/">
-            <HomeWorkIcon edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </HomeWorkIcon>
-          </Link>
-          <Typography variant="h6" className={classes.title}>
-            HoBBYDRoP
-=======
       <div className={classes.root}>
         <AppBar position="static" className={classes.style}>
           <Toolbar>
@@ -56,7 +44,6 @@ export default function ButtonAppBar() {
             </Link>
             <Typography variant="h6" className={classes.title}>
               HoBBYDRoP
->>>>>>> a83a163f6b64bd0cb77f5819e0540b5fa9b2cd5e
           </Typography>
             <Link to="/login"><Button color="inherit">Login/Signup</Button></Link>
           </Toolbar>
